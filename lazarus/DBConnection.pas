b302ee6173b35e262045c480806e90a79d728c81
Michael Brunklaus <cnmicha@users.noreply.github.com>
2017-03-11 13:33:03 +0100

    fixed #134 getStudentById

--- lazarus/DBConnection.pas
+++ lazarus/DBConnection.pas
@@ -340,7 +340,7 @@ begin
   setStudentFields(arr, True);
 
   Result := nil;
-  if (length(arr) = 0) then
+  if (length(arr) > 0) then
     Result := arr[0];
 end;
 
@@ -758,7 +758,7 @@ begin
   setBookFields(arr, True);
 
   Result := nil;
-  if (length(arr) = 0) then
+  if (length(arr) > 0) then
     Result := arr[0];
 end;
 
@@ -979,7 +979,7 @@ begin
   setBooktypeFields(arr, True);
 
   Result := nil;
-  if (length(arr) = 0) then
+  if (length(arr) > 0) then
     Result := arr[0];
 end;
 
